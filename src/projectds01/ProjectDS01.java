1910580a304c0dbb7013636aafec2b6fd35cff97
DELL <DELL@ASAZ>
2019-09-28 00:18:07 -0500

    v01 Myrian

--- src/projectds01/ProjectDS01.java
+++ src/projectds01/ProjectDS01.java
@@ -19,6 +19,10 @@ public class ProjectDS01 {
         System.out.println("ggggg");
         System.out.println("------------");
         ///GFGCC
+        int a=5;
+        int b=8;
+        System.out.println("; "+(a+b));
+        
     }
     
 }
